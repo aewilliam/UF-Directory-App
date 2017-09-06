cae5dc88cb794b19a2ac555d6bd2047b06209e76
Andre Williams <ae.williams@ufl.edu>
2017-09-06 12:56:49 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://<ae.williams>:<yungchoponthabeat>@ds123534.mlab.com:23534/uf-directory-app-listings', //place the URI of your mongo database here.
+  }
+};
