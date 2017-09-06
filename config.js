7db58db2f3333a6ffd82bece7f120024b5847ab6
Andre Williams <ae.williams@ufl.edu>
2017-09-06 15:06:19 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: {
-    uri:'mongodb://<ae.williams>:<yungchoponthabeat>@ds123534.mlab.com:23534/uf-directory-app-listings', //place the URI of your mongo database here.
-  }
-};
